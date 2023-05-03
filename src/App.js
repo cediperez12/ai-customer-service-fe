a117f6d137d5c5383505c714adc88f0a285ebad7
cediperez12 <kirisakinoshka@gmail.com>
2023-05-03 14:22:40 +0800

    implemented ai

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect, useRef } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [chats, setChats] = useState([])
+  const [text, setText] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const input = useRef(null)
+
+  useEffect(() => {
+    input.current.focus()
+  }, [isLoading])
+
+  const sendMessage = (message, query) => {
+    const newMessages = [
+      {
+        bubble: 'User',
+        message: query,
+      },
+      {
+        bubble: 'CHIM.AI',
+        message: message,
+      },
+    ]
+
+    setChats([...chats, ...newMessages])
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    console.log('submitted')
+    console.log('text', text)
+
+    getChimResponse(text)
+  }
+
+  const getChimResponse = async (text) => {
+    try {
+      setIsLoading(true)
+      const response = await axios.get(
+        `http://192.168.100.123:81/askchim?query=${text}`,
+        {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      console.log('response', response)
+
+      const { response: message, query } = response.data
+      sendMessage(message, query)
+
+      setText('')
+    } catch (error) {
+      console.log('error', error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="title">
+        <h1>CHIM.AI CUSTOMER SERVICE</h1>
+      </div>
+      <div className="chat-box">
+        {chats.map((ch, index) => (
+          <p key={index}>
+            <b>{ch.bubble}: </b>
+            {ch.message}
+          </p>
+        ))}
+      </div>
+      <div className="text-box">
+        <form action="#" className="form" onSubmit={onSubmit}>
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Ask something to CHIM! Ex. How do I order?"
+            value={text}
+            ref={input}
+            onChange={(e) => setText(e.target.value)}
+            disabled={isLoading}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
