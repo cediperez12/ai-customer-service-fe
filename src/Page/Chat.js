cb4082be3c68bee17996f1d3459499cf827cb6e8
mbaluyot <m.gwynethbaluyot@gmail.com>
2023-05-04 17:04:58 +0800

    fixed some UI

--- src/Page/Chat.js
+++ src/Page/Chat.js
@@ -1,46 +1,56 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import SupportAgentIcon from '@mui/icons-material/SupportAgent';
-import { IconButton, InputAdornment, TextField } from '@mui/material';
-import SendIcon from '@mui/icons-material/Send';
-import Conversation from './Conversation';
-import axios from 'axios';
-import loadingGif from '../assets/loading.gif'  
-  export default function Chat() {
+import * as React from 'react'
+import {
+  Box,
+  CssBaseline,
+  Typography,
+  TextField,
+  InputAdornment,
+  IconButton,
+  AppBar,
+  Toolbar,
+} from '@mui/material'
+import {
+  SupportAgent as SupportAgentIcon,
+  ExpandMore as ExpandMoreIcon,
+} from '@mui/icons-material'
+import SendIcon from '@mui/icons-material/Send'
+import Conversation from './Conversation'
 
-    // * Sample Data * ----------------
-    const  [messages, setMessages] = React.useState([]);
+import axios from 'axios'
+import loadingGif from '../assets/loading.gif'
 
-    const [newMessage, setNewMessage] =  React.useState('');
-    const [isLoading, setIsLoading] = React.useState(false);
-   
-    const handleSendMessage = () => {
-      getChimResponse();
-      setNewMessage('');
+export default function Chat() {
+  // * Sample Data * ----------------
+  const [messages, setMessages] = React.useState([])
+  const [newMessage, setNewMessage] = React.useState('')
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  const handleSendMessage = () => {
+    getChimResponse()
+    setNewMessage('')
+  }
+
+  // * Enter key bind  * ----------------
+  const handleEnterKey = (event) => {
+    if (event.keyCode === 13 && newMessage.length > 0) {
+      handleSendMessage()
     }
+  }
 
-    // * Enter key bind  * ----------------
-    const handleEnterKey = (event) => {
-      if(event.keyCode === 13 && newMessage.length > 0){
-        handleSendMessage()
-      }
-    }
-
-    // * Auto scroll * ----------------
-    const windowRef = React.useRef(null);
-    React.useEffect(() => {
-      windowRef.current.scrollIntoView({ behavior: 'smooth' });
-    }, [messages]);
+  // * Auto scroll * ----------------
+  const windowRef = React.useRef()
+  React.useEffect(() => {
+    if (!windowRef.current) return
+    windowRef.current.scrollIntoView({ behavior: 'smooth' })
+  }, [messages])
 
   const getChimResponse = async (text) => {
     try {
-      setIsLoading(true);
+      setIsLoading(true)
       const response = await axios.get(
-        `https://dcb5-2001-4451-70f-2600-b045-e289-ff25-367.ngrok-free.app/askchim?query=` + newMessage,
+        `https://dcb5-2001-4451-70f-2600-b045-e289-ff25-367.ngrok-free.app/askchim?query=` +
+          newMessage,
         {
           headers: {
             'Access-Control-Allow-Origin': '*',
@@ -50,14 +60,13 @@ import loadingGif from '../assets/loading.gif'
         }
       )
       console.log('response', response.data)
-      setMessages([...messages, response.data]);
-      setIsLoading(false);
-     
+      setMessages([...messages, response.data])
+      setIsLoading(false)
     } catch (error) {
       console.log('error', error)
-      setIsLoading(false);
+      setIsLoading(false)
     } finally {
-      setIsLoading(false);
+      setIsLoading(false)
     }
   }
 
@@ -65,67 +74,110 @@ import loadingGif from '../assets/loading.gif'
   //   console.log(messages);
   // },[messages])
 
-    return (
-      <React.Fragment>
-        <CssBaseline />
-        <Box sx={{
-              position: 'absolute', 
-              bottom: '0', 
-              right: '0',
-              marginRight: 2,
-              display: "flex",
-              height: 570,
-              backgroundColor: '#F5F5F9',
-              width: 380,
-              flexDirection: "column",
-        }}>
-      
-          <Box sx={{
-              display: "flex",
-              height: 'auto',
-              width: 'auto',
-              flexDirection: "column",
-              alignItems: "center",
-              overflow: "hidden",
-              '&::-webkit-scrollbar': { display: 'none' },
-              overflowY: "scroll",
-        }}>
-          <AppBar position='sticky' style={{marginBottom: '20px'}}>
-            <Toolbar style={{backgroundColor: '#014F41'}}>
-              <SupportAgentIcon/> <span> &nbsp; </span>
-              <Typography variant="h6" component="div">
-                Chat Support
-              </Typography>
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <IconButton
+        onClick={() => {
+          setIsOpen((current) => !current)
+        }}
+        size="large"
+        sx={{
+          position: 'fixed',
+          bottom: 16,
+          right: 16,
+          zIndex: 1000,
+          backgroundColor: '#014F41',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#014F41',
+          },
+        }}
+      >
+        {isOpen ? <ExpandMoreIcon /> : <SupportAgentIcon />}
+      </IconButton>
+      {isOpen && (
+        <Box
+          sx={{
+            position: 'fixed',
+            mb: 10,
+            bottom: 0,
+            right: 16,
+            transition: 'all 0.3s ease-in-out',
+            transform: `translateY(${isOpen ? 0 : '100%'})`,
+            display: 'flex',
+            height: 500,
+            backgroundColor: '#f9f9f5',
+            width: 350,
+            flexDirection: 'column',
+            overflow: 'hidden',
+          }}
+        >
+          <AppBar position="static">
+            <Toolbar
+              style={{
+                backgroundColor: '#014F41',
+              }}
+            >
+              <Box sx={{ mr: 2 }}>
+                <SupportAgentIcon fontSize={'large'} />
+              </Box>
+              <Typography variant="h6">Chat Support</Typography>
             </Toolbar>
           </AppBar>
 
           {/* ---------------- *  Conversation  * ---------------- */}
-           <Conversation messages = { messages }/>
-           <div ref={windowRef}/>
-         {/* <Box sx={{display: "flex", justifySelf:'flex-end', right: 'auto', left:'auto', width: 380, bottom: 0, position:'fixed',}}> */}
-         <TextField  sx={{  color:'#ffffff'}} 
-                  className  = 'textField'
-                  disabled = { isLoading ?  true : false }
-                  value      = { newMessage }
-                  onKeyDown  = { handleEnterKey }
-                  label      = 'Chat here'
-                  fullWidth
-                  onChange   = { (e) => { setNewMessage(e.target.value) }}
-                  InputProps = {{
-                    endAdornment:(
-                      <InputAdornment position = 'end'>
-                      <IconButton edge = 'start'   onClick ={ handleSendMessage } disabled = { newMessage ?  false : true }>
-                        {isLoading ? <img src={loadingGif} style={{width: 50, height: 50}} alt='loading'/>  : <SendIcon style={{ color: newMessage ? '#014F41' : '#828F8D'}} /> }
-                      </IconButton>
-                    </InputAdornment>
-                              ) 
-              }}/>
-         {/* </Box> */}
-          
+          <Box
+            sx={{
+              mb: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              height: 700,
+              overflow: 'hidden',
+              overflowY: 'scroll',
+            }}
+          >
+            <Conversation messages={messages} />
+            <div ref={windowRef} />
           </Box>
-         
-        
-          </Box>
-      </React.Fragment>
-    );
-  }
+
+          <TextField
+            sx={{ color: '#ffffff', mt: '1px', py: 1, px: 1 }}
+            className="textField"
+            disabled={isLoading ? true : false}
+            value={newMessage}
+            onKeyDown={handleEnterKey}
+            label="Chat here"
+            fullWidth
+            onChange={(e) => {
+              setNewMessage(e.target.value)
+            }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    edge="start"
+                    onClick={handleSendMessage}
+                    disabled={newMessage ? false : true}
+                  >
+                    {isLoading ? (
+                      <img
+                        src={loadingGif}
+                        style={{ width: 50, height: 50 }}
+                        alt="loading"
+                      />
+                    ) : (
+                      <SendIcon
+                        style={{ color: newMessage ? '#014F41' : '#828F8D' }}
+                      />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+      )}
+    </React.Fragment>
+  )
+}
